27d8f84fff700a911af2178cfe5ef577c97f8ddf
samuel-reedy <64679797+samuel-reedy@users.noreply.github.com>
2022-07-27 18:05:11 +1200

    Home, Limit Switches, Movement Functions

--- testingLimitSwitches.ino
+++ testingLimitSwitches.ino
@@ -1,61 +1,135 @@
+
 // Limit switch pins
 const int leftSwitch = 18, rightSwitch = 19, topSwitch = 21, bottomSwitch = 20;
 
-// Motor pins
+// Motor 1 pins
 const int motorOneDir = 4, motorOneSpeed = 5;
+// Motor 2 pins
+const int motorTwoDir = 7, motorTwoSpeed = 6;
 
 volatile bool isRunning;
-
-int motorSpeed;
-float initialTime;
+volatile bool foundHome = false;
 
 void setup() {
   isRunning = true;
-  motorSpeed = 0;
   
   Serial.begin(9600);
-  // Limit Switche setups
-  pinMode(leftSwitch, INPUT);
-  pinMode(rightSwitch, INPUT);
-  pinMode(topSwitch, INPUT);
-  pinMode(bottomSwitch, INPUT);
+  // Limit Switch setups
+  pinMode(leftSwitch, INPUT_PULLUP);
+  pinMode(rightSwitch, INPUT_PULLUP);
+  pinMode(topSwitch, INPUT_PULLUP);
+  pinMode(bottomSwitch, INPUT_PULLUP);
 
   // Motor pin setup
   pinMode(motorOneDir, OUTPUT);
-
-  // Interrupt Setup
-
-  cli();
+  pinMode(motorTwoDir, OUTPUT);
   
-  attachInterrupt(digitalPinToInterrupt(leftSwitch), LimitSwitch, RISING);
-  attachInterrupt(digitalPinToInterrupt(rightSwitch), LimitSwitch, RISING);
-  attachInterrupt(digitalPinToInterrupt(topSwitch), LimitSwitch, RISING);
-  attachInterrupt(digitalPinToInterrupt(bottomSwitch), LimitSwitch, RISING);
-  
-  digitalWrite(motorOneDir, LOW);
-  analogWrite(motorOneSpeed, 0);
+  Stop();
 
-  initialTime = millis();
+  attachInterrupt(digitalPinToInterrupt(rightSwitch), SwitchLimit, RISING); 
+  attachInterrupt(digitalPinToInterrupt(leftSwitch), SwitchLimit, RISING); 
+  attachInterrupt(digitalPinToInterrupt(topSwitch), SwitchLimit, RISING); 
+  attachInterrupt(digitalPinToInterrupt(bottomSwitch), SwitchLimit, RISING); 
+  
   
 }
 
 void loop() {
-  if (motorSpeed < 150){
-    motorSpeed++;
+  if (!foundHome){
+    FindHome();
   }
+
+  //CheckLimits();
+
   if (isRunning){
-    analogWrite(motorOneSpeed, motorSpeed);
+    RightDiagonal(100, HIGH);
   }else{
-    analogWrite(motorOneSpeed, 0);
+    Stop();
   }
-  Serial.println(digitalRead(topSwitch));
+  Serial.println(isRunning);
   delay(10);
-  if ((millis() - initialTime) > 1000){
-    sei();
+}
+
+void FindHome(){
+  bool foundLeft = false, foundBottom = false;
+
+  // Finding the left limit switch
+  while(!foundLeft){
+    Horizontal(100, HIGH);
+    if (digitalRead(leftSwitch)){
+      Serial.println(digitalRead(leftSwitch));
+      foundLeft = true;
+      Stop();
+    }
+  }
+  delay(100);
+  while(digitalRead(leftSwitch)){
+    Horizontal(100, LOW);
+  }
+  Stop();
+  delay(100);
+  
+  // Finding the bottom limit switch
+  while(!foundBottom){
+    Vertical(100, HIGH);
+    if (digitalRead(bottomSwitch)){
+      foundBottom = true;
+      Stop();
+    }
+  }  
+  delay(100);
+   while(digitalRead(bottomSwitch)){
+    Vertical(100, LOW);
+  }
+  Stop();
+  delay(100);
+  Stop();
+  foundHome = true;
+}
+
+void CheckLimits(){
+  if (digitalRead(topSwitch) || digitalRead(bottomSwitch) || digitalRead(rightSwitch) || digitalRead(leftSwitch)){
+    isRunning = false; 
   }
 }
 
-void LimitSwitch(){
-  Serial.println("Interrupting");
-  isRunning = false;
+
+void SwitchLimit(){
+  if(foundHome){
+    isRunning = false;
+    Serial.println("Right Interrupt");
+  }
+}
+
+void Horizontal(int motorSpeed, int horDir){
+    analogWrite(motorOneSpeed, motorSpeed);
+    analogWrite(motorTwoSpeed, motorSpeed);
+    digitalWrite(motorOneDir, horDir);
+    digitalWrite(motorTwoDir, horDir);
+}
+
+void Vertical(int motorSpeed, int verDir){
+    analogWrite(motorOneSpeed, motorSpeed);
+    analogWrite(motorTwoSpeed, motorSpeed);
+    digitalWrite(motorOneDir, !verDir);
+    digitalWrite(motorTwoDir, verDir);
+}
+
+void LeftDiagonal(int motorSpeed, int verDir){
+    analogWrite(motorOneSpeed, motorSpeed*verDir);
+    analogWrite(motorTwoSpeed, motorSpeed*!verDir);
+    digitalWrite(motorOneDir, !verDir);
+    digitalWrite(motorTwoDir, verDir);
+}
+
+void RightDiagonal(int motorSpeed, int verDir){
+    analogWrite(motorOneSpeed, motorSpeed*!verDir);
+    analogWrite(motorTwoSpeed, motorSpeed*verDir);
+    digitalWrite(motorOneDir, !verDir);
+    digitalWrite(motorTwoDir, verDir);
+}
+
+void Stop(){
+    analogWrite(motorOneSpeed, 0);
+    analogWrite(motorTwoSpeed, 0);
 }
