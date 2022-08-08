3712c2f94fc900f13e6d4e86baddbfdfb76f1a4f
samuel-reedy <samreedy7@gmail.com>
2022-08-09 10:20:09 +1200

    Further PID Implementation
    Updated the HoriztonalDistance function to MoveDistance, now takes an x distance and a y distance.
    Created a find home V2
    Changed check limits to move the head away from the edge when called

---
+++ testingLimitSwitches/testingLimitSwitches.ino
@@ -0,0 +1,384 @@
+// ######################################################### PID CLASS #########################################################
+// Can be instantiated with PID values and its CalculateEffort can be called
+// by passing the current error, the saturation limit for the P effort, and dt
+// dt --> is the time between the last CalculateEffort call, and the current time
+class PIDController{
+  private:
+    // Variables
+    float Kp;
+    float Ki;
+    float Kd;
+    
+    int sumError;
+    int prevError;
+    float P, I, D;
+  public:
+    // Contructor
+    PIDController(float Kp, float Ki, float Kd){
+      this->Kp = Kp;
+      this->Ki = Ki;
+      this->Kd = Kd;
+
+      this->sumError = 0;
+      this->prevError = 0;
+      this->P = 0;
+      this->I = 0;
+      this->D = 0;
+    }
+
+    // Function to calculate the control effort
+    float CalculateEffort(float error, int saturationLimit, float dt){
+      // Proportional Control Effort
+      P = Kp * error;
+      int sign = (P > 0) - (P < 0);
+      // Saturating the Proportional Control Effort
+      P = P > saturationLimit ? saturationLimit : P < -saturationLimit ? -saturationLimit : P;
+
+      // Anti Integral windup
+      if (abs(P) < saturationLimit){
+        sumError += error * dt;  
+      }
+      // Integral Control Effort
+      I = Ki * sumError;
+
+      // Derivative Control Effort
+      if (dt != 0){
+        D = Kd * ((error - prevError) / dt);
+      }
+      
+      // Updating the previous Error
+      prevError = error;
+
+      float u = P + I + D;
+
+      u = abs(u) > 230 ? 230 * ((u > 0) - (u < 0)) : u;
+      // Returning the total control effort
+      return u;
+    }
+
+    // Function to reset sum and previous error
+    void ResetPID(){
+      this->sumError = 0;
+      this->prevError = 0;
+    }
+};
+
+
+// ######################################################### ARDUINO CODE SETUP #########################################################
+
+#define ACCELERATION_TIME 100
+
+#define CALCOUNT 2300.0
+#define CALDIST 50.0
+#define PULLEYDIAM (CALDIST / (PI * (CALCOUNT / 2064.0)))
+
+// Limit switch pins
+const int leftSwitch = 18, rightSwitch = 19, topSwitch = 21, bottomSwitch = 20;
+
+// Motor 1 pins
+const int motorRDirPin = 4, motorRSpeedPin = 5;
+// Motor 2 pins
+const int motorLDirPin = 7, motorLSpeedPin = 6;
+
+volatile bool isRunning;
+volatile bool foundHome = false;
+
+// Declaring encoder a pins for left and right motors
+const int encoderAL = 2, encoderAR = 3; 
+
+// Declaring encoder b pins for left and right motors
+const int encoderBL = 46, encoderBR = 44;
+
+// Declaring position on board
+volatile int xCoord, yCoord;
+
+// For testing
+volatile int posL = 0, posR = 0;
+
+/*
+ * RECTANGLE WIDTH AND HEIGHT, CIRCLE DIAMETER
+ */
+volatile const int rectWidth = 5;
+volatile const int rectHeight = 10;
+
+volatile const int circDiam = 5;
+
+// ######################################################### SETUP #########################################################
+
+void setup() {
+  isRunning = true;
+  
+  Serial.begin(9600);
+  // Limit Switch setups
+  pinMode(leftSwitch, INPUT_PULLUP);
+  pinMode(rightSwitch, INPUT_PULLUP);
+  pinMode(topSwitch, INPUT_PULLUP);
+  pinMode(bottomSwitch, INPUT_PULLUP);
+
+  // Motor pin setup
+  pinMode(motorRDirPin, OUTPUT);
+  pinMode(motorLDirPin, OUTPUT);
+
+  // Encoder pin setup
+  pinMode(encoderAL, INPUT_PULLUP);
+  pinMode(encoderAR, INPUT_PULLUP);
+  pinMode(encoderBL, INPUT_PULLUP);
+  pinMode(encoderBR, INPUT_PULLUP);
+  
+  Stop();
+
+//  while(1){
+//    RightDiagonal(100, HIGH);
+//    delay(10);
+//  }
+
+  FindHome();
+  
+  // Allowing encoder interrupts to occur
+  // Encoder A motorL/R interrupts
+  cli();
+  attachInterrupt(digitalPinToInterrupt(encoderAR), EncoderRInterrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(encoderAL), EncoderLInterrupt, RISING);
+  sei();
+}
+
+// ######################################################### LOOP #########################################################
+
+void loop() {
+  //CheckLimits();
+  
+  posR = 0;
+  posL = 0;
+
+  for(int i=0; i<300; i++) {
+    RightDiagonal(100, HIGH);
+    delay(10);
+    //CheckLimits();
+  }
+  Stop();
+
+  delay(10000);
+
+  MoveDistance(40,0,1,0,0, true);
+
+  delay(10);
+}
+
+// ######################################################### FUNCTIONS #########################################################
+// =========================== FIND HOME ===========================
+void FindHome(){
+  //Serial.println("Entering home");
+  bool foundLeft = false, foundBottom = false;
+
+  // Finding the left limit switch
+  while(!foundLeft){
+    Horizontal(100, LOW);
+    Serial.println(digitalRead(leftSwitch));
+    if (digitalRead(leftSwitch)){
+      foundLeft = true;
+      Stop();
+    }
+    delay(10);
+  }
+  delay(500);
+  while(digitalRead(leftSwitch)){
+    Horizontal(100, HIGH);
+    delay(10);
+  }
+  Stop();
+  delay(500);
+  
+  // Finding the bottom limit switch
+  while(!foundBottom){
+    Vertical(100, LOW);
+    Serial.println(digitalRead(bottomSwitch));
+    if (digitalRead(bottomSwitch)){
+      foundBottom = true;
+      Stop();
+    }
+    delay(10);
+  }  
+  delay(500);
+  while(digitalRead(bottomSwitch)){
+    Vertical(100, HIGH);
+    delay(10);
+  }
+  Stop();
+  delay(1000);
+  foundHome = true;
+  Serial.println("Exiting home");
+
+  // Declaring origin
+  xCoord = 0;
+  yCoord = 0;
+  
+}
+
+void FindHomeV2(){
+  MoveDistance(-500, 0, 0.5, 0, 0, true);
+  delay(100);
+  isRunning = true;
+
+  MoveDistance(0, -500, 0.5, 0, 0, true);
+  delay(100);
+  isRunning = true;
+
+  delay(1000);
+  // Declaring origin
+  xCoord = 0;
+  yCoord = 0;
+}
+
+// =========================== CHECK LIMITS ===========================
+void CheckLimits(){
+  if (digitalRead(topSwitch) || digitalRead(bottomSwitch) || digitalRead(rightSwitch) || digitalRead(leftSwitch)){
+
+    // Move the head away from the switch
+    if(digitalRead(topSwitch)) { MoveDistance(0, -30, 0.5, 0, 0, false); }
+    if(digitalRead(bottomSwitch)) { MoveDistance(0, 30, 0.5, 0, 0, false); }
+    if(digitalRead(rightSwitch)) { MoveDistance(-30, 0, 0.5, 0, 0, false); }
+    if(digitalRead(leftSwitch)) { MoveDistance(30, 0, 0.5, 0, 0, false); }
+    
+    isRunning = false; 
+
+    //while(1) Stop();
+  }
+}
+
+// =========================== COUNT TO DISTANCE ===========================
+float CountToDistance(float count) {
+  return (PI * PULLEYDIAM * count)/2064.0; 
+}
+
+// =========================== DISTANCE TO COUNT ===========================
+float DistanceToCount(int distance) {
+  return (distance * 2064.0) / (PI * PULLEYDIAM);
+}
+
+void EncoderRInterrupt() {
+   int encoderBState = digitalRead(encoderBR);
+   encoderBState > 0 ? posR++ : posR--;
+}
+
+void EncoderLInterrupt() {
+  int encoderBState = digitalRead(encoderBL);
+  encoderBState > 0 ? posL++ : posL--;
+}
+
+// =========================== SATURATE ===========================
+float saturate(float value, float min, float max){
+  return value > max ? max : value < min ? min : value;
+}
+
+// =========================== SIGN ===========================
+// Returns the sign of a value:
+// -1 for -ve ; 0 for 0 ; 1 for +ve
+int sign(float value){
+  return ((value > 0) - (value < 0));
+}
+
+// =========================== MOVE DISTANCE ===========================
+// Requires the horizontal and vertical distance you want the head to move
+// Requires Kp, Ki, Kd
+// Checklimits bool decides whether the function will check to see if it hits the limit switch
+// (used for the FindHomeV2)
+void MoveDistance(float xDist, float yDist, float Kp, float Ki, float Kd, bool checkLimits){
+   
+    // Create the PID Controllers with the assigned values
+    PIDController motorPID(Kp, Ki, Kd);         // Motor base power PID controller
+    PIDController motorDiffPID(0.5,0,0);        // Encoder difference PID controller
+   
+    float controlEffortL, controlEffortR, controlEffortDiff, dt, motorLError, motorRError, encError;
+    float initialTime = millis();
+    float prevTime = millis();
+    float exitTime = 0;
+
+    posL = 0;       // Reset left encoder
+    posR = 0;       // Reset right encoder
+
+    float leftDistFromKin = (xDist + yDist); 
+    float rightDistFromKin = (xDist - yDist);
+
+    float motorLEncTarget = DistanceToCount(leftDistFromKin);
+    float motorREncTarget = DistanceToCount(rightDistFromKin);
+
+    float encRatio = motorLEncTarget / motorREncTarget;
+
+    bool canExit = false;
+   
+    do{
+      if (checkLimits) CheckLimits();
+      dt = millis() - prevTime;
+
+      // Calculating PID control efforts for the motor base powers and the encoder variation motor powers
+      motorLError = motorLEncTarget - posL;
+      motorRError = motorREncTarget - posR;
+      encError = posL - (posR * encRatio);                                    // Applying the encoder ration to the error calc
+
+      controlEffortL = motorPID.CalculateEffort(motorLError, 235, dt);        // Left motor control effort calculation
+      controlEffortR = motorPID.CalculateEffort(motorRError, 235, dt);        // Right motor control effort calculation
+      controlEffortDiff = motorDiffPID.CalculateEffort(encError, 40, dt);     // Encoder error control effort calculation
+
+      controlEffortL = saturate(controlEffortL - (controlEffortDiff / 2.0),-255, 255);    
+      controlEffortR = saturate(controlEffortR + (controlEffortDiff / 2.0),-255, 255);
+
+      analogWrite(motorLSpeedPin, abs(controlEffortL));
+      analogWrite(motorRSpeedPin, abs(controlEffortR));
+
+      digitalWrite(motorLDirPin, sign(controlEffortL) == -1 ? 0 : 1);         // Setting the motor direction to either 0 or 1 depending on the sign
+      digitalWrite(motorRDirPin, sign(controlEffortR) == -1 ? 0 : 1);
+     
+      // Exit Condition (NEEDS MORE)
+      if (abs(motorLError) <= 5 && abs(motorRError) <= 5){
+        exitTime += dt;
+      }else{
+        exitTime = 0;
+      }
+
+      if (exitTime > 200){
+        canExit = true;
+      }
+
+      prevTime = millis();
+      if (checkLimits) CheckLimits();
+      delay(1);
+
+    } while(!canExit && isRunning);
+    Stop();
+    delay(10000);
+}
+
+// =========================== BASIC MOTOR FUNCTIONS ===========================
+void Horizontal(int motorSpeed, int horDir){
+    analogWrite(motorRSpeedPin, motorSpeed);
+    analogWrite(motorLSpeedPin, motorSpeed);
+    digitalWrite(motorRDirPin, horDir);
+    digitalWrite(motorLDirPin, horDir);
+}
+
+void Vertical(int motorSpeed, int verDir){
+    analogWrite(motorRSpeedPin, motorSpeed);
+    analogWrite(motorLSpeedPin, motorSpeed);
+    digitalWrite(motorRDirPin, !verDir);
+    digitalWrite(motorLDirPin, verDir);
+}
+
+void LeftDiagonal(int motorSpeed, int verDir){
+    analogWrite(motorRSpeedPin, motorSpeed*verDir);
+    analogWrite(motorLSpeedPin, motorSpeed*!verDir);
+    digitalWrite(motorRDirPin, !verDir);
+    digitalWrite(motorLDirPin, verDir);
+}
+
+void RightDiagonal(int motorSpeed, int verDir){
+    analogWrite(motorRSpeedPin, motorSpeed*!verDir);
+    analogWrite(motorLSpeedPin, motorSpeed*verDir);
+    digitalWrite(motorRDirPin, !verDir);
+    digitalWrite(motorLDirPin, verDir);
+}
+
+void Stop(){
+    analogWrite(motorRSpeedPin, 0);
+    analogWrite(motorLSpeedPin, 0);
+}
+
