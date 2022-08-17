6fa28a14b69e153a8aadcb731adeea3be252afff
samuel-reedy <samreedy7@gmail.com>
2022-08-17 18:18:48 +1200

    Circle Improved

--- MECHENG306/MECHENG306Circle.ino
+++ MECHENG306/MECHENG306Circle.ino
@@ -2,7 +2,8 @@
 #include "MECHENG306BasicFunctions.h"
 
 void CircleV3(float radius, float period){
-  PIDController motorPID(2,0.1,0.1);
+  PIDController motorPIDL(1,0,0);
+  PIDController motorPIDR(1,0,0);
 
   float dt;
   float prevTime = millis();
@@ -11,21 +12,44 @@ void CircleV3(float radius, float period){
   posR = 0;
   posL = 0;
   do{
+    CheckLimits();
     dt = (millis() - prevTime)/1000.0;
     prevTime = millis();
     T += dt;
-    float target = DistanceToCount(radius * sin((2*PI*T)/period));
-    Serial.print(target);
-    Serial.print(" \t ");
-    float error = target - posR;
-    Serial.print(posR);
-    Serial.print(" \t ");
-    float controlEffort = motorPID.CalculateEffort(error,190,dt);
-    Serial.println(controlEffort);
-    controlEffort += 60;
-    analogWrite(motorRSpeedPin, abs(controlEffort));
-    digitalWrite(motorRDirPin, sign(controlEffort) == -1 ? 1 : 0);
+    
+    float targetH = DistanceToCount(radius * sin((2*PI*T)/period));    
+    float targetV = DistanceToCount(radius * cos((2*PI*T)/period));
+
+    float targetL = targetH + targetV;
+    float targetR = targetH - targetV;
+    
+    float errorL = targetL - posL;
+    float errorR = targetR - posR;
+   
+    float controlEffortL = motorPIDL.CalculateEffort(errorL,190,dt) + 60;
+    float controlEffortR = motorPIDR.CalculateEffort(errorR,190,dt) + 60;
+
+    analogWrite(motorLSpeedPin, abs(controlEffortL));
+    analogWrite(motorRSpeedPin, abs(controlEffortR));
+
+    digitalWrite(motorLDirPin, sign(controlEffortL) == -1 ? 0 : 1);
+    digitalWrite(motorRDirPin, sign(controlEffortR) == -1 ? 0 : 1);
+
+    Serial.print(targetL);
+    Serial.print("\t");
+
+    Serial.print(targetR);
+    Serial.print("\t");
+
+    Serial.print(posL);
+    Serial.print("\t");
+
+    Serial.println(posR);
+   
+  
+    
     delay(10);
+    
 
   }while(1);
 
